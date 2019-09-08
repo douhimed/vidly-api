2eea8d3e6015206b39395904998a0d41d0742fda
douhimed <mr.douhi.med@gmail.com>
2019-09-09 00:02:04 +0100

    Testing the auth middleware

--- tests/unit/middlewares/auth.test.js
+++ tests/unit/middlewares/auth.test.js
@@ -1,5 +1,23 @@
+const { User } = require("../../../models/user");
+const mongoose = require("mongoose");
+const auth = require("../../../middlewares/auth");
+
 describe("The Auth Middleware", () => {
-  it("For passing ", () => {
-    expect(1).toBeTruthy();
+  it("should populate req.user with the payload of a valid JWT ", () => {
+    const user = {
+      _id: mongoose.Types.ObjectId().toHexString(),
+      isAdmin: true
+    };
+
+    const token = new User(user).generateAuthToken();
+    const req = {
+      header: jest.fn().mockReturnValue(token)
+    };
+    const resp = {};
+    const next = jest.fn();
+
+    auth(req, resp, next);
+
+    expect(req.user).toMatchObject(user);
   });
 });
